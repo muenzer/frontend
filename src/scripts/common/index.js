c11936b9caab1ce5bce6ab413366981ddc4f8687
Chris Muenzer <chris.muenzer.cm1@roche.com>
2018-01-05 14:33:25 +0100

    cleaned up modules

--- src/scripts/common/index.js
+++ src/scripts/common/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var api = require('./api')
+var user = require('./user')
 
-var app = require('angular').module('common', [api])
+var app = require('angular').module('common', [api, user])
 
 module.exports = app.name
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,6 @@
 
 var angular = require('angular')
 var ngMaterial = require('angular-material')
+var common = require('./common')
 
-angular.module('app', [ngMaterial])
+angular.module('app', [ngMaterial, common])
