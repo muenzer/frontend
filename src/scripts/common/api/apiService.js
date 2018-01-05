aef084a3736175e374f9b047144904037804da0d
Chris Muenzer <chris.muenzer.cm1@roche.com>
2018-01-05 15:28:23 +0100

    added get user info

--- src/scripts/common/api/apiService.js
+++ src/scripts/common/api/apiService.js
@@ -2,13 +2,18 @@
 
 var apiService = function (Restangular) {
   var service = {
-    login: login
+    login: login,
+    userInfo: userInfo
   }
 
   return service
 
   function login (credintials) {
     return Restangular.all('users').all('login').post(credintials)
+  }
+
+  function userInfo () {
+    return Restangular.all('users').getList()
   }
 }
 
