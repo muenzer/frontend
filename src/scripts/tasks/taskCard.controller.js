4540f51fde6f5b2c4281eba32b6c0d2d94c49ee5
Chris Muenzer <chris.muenzer.cm1@roche.com>
2018-01-09 16:34:34 +0100

    added task functions

--- src/scripts/tasks/taskCard.controller.js
+++ src/scripts/tasks/taskCard.controller.js
@@ -4,6 +4,12 @@ module.exports = function TaskCardController () {
   var $ctrl = this
   console.log($ctrl.task)
 
+  $ctrl.update = function (task) {
+    return task.then(function (response) {
+      $ctrl.task = response
+    })
+  }
+
   $ctrl.$onInit = function () {
     $ctrl.expanded = false
   }
